a46ea49f99eb3269253c952094fd5826f558bada
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-05-24 17:42:58 +0100

    Created generic class Guarda3 and its constructor

---
+++ MyGenericClass/Guarda3.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyGenericClass
+{
+    public class Guarda3<T>
+    {
+        private T val1;
+        private T val2;
+        private T val3;
+        public Guarda3()
+        {
+            val1 = default;
+            val2 = default;
+            val3 = default;
+        }
+ }
+}
